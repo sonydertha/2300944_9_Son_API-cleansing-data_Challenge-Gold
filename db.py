7bd24f5786cbdef146cc587bde9c2cf4e915892a
sonydertha <sonyderthas@gmail.com>
2023-06-10 19:11:08 +0700

    add db.py

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ including :
 2. Create table Kamus Alay & Abusive
 3. Insert Result of data cleansing 
 """
-
+import pandas as pd
 import sqlite3
 
 def create_connection():
@@ -13,4 +13,24 @@ def create_connection():
     return conn
 
 def insert_dictionary_to_db(conn):
+    abusive_csv_data = "csv_data/abusive.csv"
+    alay_csv_data = "csv_data/new_kamusalay.csv"
+
+    # read csv file to dataframe
+    print ("read csv file to dataframe...")
+    df_abusive = pd.read_csv(abusive_csv_data)
+    df_alay = pd.read_csv(alay_csv_data)
+    print ("read csv file to dataframe success!!")
+
+    # standardize column name
+    print ("standardize column name...")
+    df_abusive.columns = ['word']
+    df_alay.columns = ['alay_word','formal_word']
+    print ("standardize column name success !!")
+
+    # insert dataframe to database
+    print ("insert dataframe to database...")
+    df_abusive.to_sql  ('abusive',conn, if_exists='replace',index=False)
+    df_alay.to_sql  ('alay',conn, if_exists='replace',index=False)
+    print ("insert dataframe to database success")
     
